b2834b27e40ec19dde5a269f6441802c7b43ced5
Alicja <k2l8m11n2@gmail.com>
2019-09-19 16:05:39 +0200

    Modify entries list to use the new API

--- src/utils/format.js
+++ src/utils/format.js
@@ -6,38 +6,28 @@ function diff(x, y) {
   const diff = new Date(+x).valueOf() - new Date(+y).valueOf();
   const hours = Math.floor(diff / (60 * 60 * 1000));
   const minutes = z(Math.floor((diff / (60 * 1000)) % 60));
-  const seconds = z(Math.floor((diff / 1000) % 60));
-  if (hours < 100) {
-    return `${hours}h ${minutes}m ${seconds}s`;
-  } else {
-    return `${hours}h ${minutes}m`;
-  }
+  return `${hours}h ${minutes}m`;
 }
 
 function date(x) {
   const t = new Date(x);
-  return (
-    t.getDate() +
-    "." +
-    z(t.getMonth() + 1) +
-    "." +
-    t.getFullYear() +
-    " " +
-    z(t.getHours()) +
-    ":" +
-    z(t.getMinutes())
-  );
+  return t.getDate() + "." + z(t.getMonth() + 1) + "." + t.getFullYear();
+}
+
+function time(x) {
+  const t = new Date(x);
+  return z(t.getHours()) + ":" + z(t.getMinutes());
 }
 
 function entryList(l) {
-  const ll = chunk(l, 2)
+  const ll = l
     .map(x => {
       return {
-        in: date(x[0].time),
-        out: x[1] ? date(x[1].time) : null,
-        duration: x[1]
-          ? diff(x[1].time, x[0].time)
-          : diff(Date.now(), x[0].time)
+        date: date(x.from * 1000),
+        from: time(x.from * 1000),
+        to: x.valid ? time(x.to * 1000) : "XXXX",
+        duration: x.valid ? diff(x.to * 1000, x.from * 1000) : "Xh XXm",
+        valid: x.valid
       };
     })
     .reverse();
