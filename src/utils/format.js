ee2cf7d7b5a9e04bac3a71ed20e4b7963aabc0fb
Autoclave <autoclave@firemail.cc>
2019-09-27 16:48:42 +0200

    Add grouping

--- src/utils/format.js
+++ src/utils/format.js
@@ -2,10 +2,9 @@ const chunk = require("lodash.chunk");
 
 const z = x => x.toString().padStart(2, 0);
 
-function diff(x, y) {
-  const diff = new Date(+x).valueOf() - new Date(+y).valueOf();
-  const hours = Math.floor(diff / (60 * 60 * 1000));
-  const minutes = z(Math.floor((diff / (60 * 1000)) % 60));
+function duration(x) {
+  const hours = Math.floor(x / (60 * 60 * 1000));
+  const minutes = z(Math.floor((x / (60 * 1000)) % 60));
   return `${hours}h ${minutes}m`;
 }
 
@@ -19,23 +18,60 @@ function time(x) {
   return z(t.getHours()) + ":" + z(t.getMinutes());
 }
 
+function ms(x) {
+  return x * 1000;
+}
+
 function entryList(l) {
   const ll = [];
   for (const [day, ens] of Object.entries(l)) {
-    ll.push({
-      day,
-      entries: ens
-        .map(x => ({
-          date: date(x.from * 1000),
-          from: time(x.from * 1000),
-          to: x.valid ? time(x.to * 1000) : "XXXX",
-          duration: x.valid ? diff(x.to * 1000, x.from * 1000) : "Xh XXm",
-          valid: x.valid
-        }))
-        .reverse()
-    });
+    const x = {};
+    x.date = date(ms(day));
+
+    x.valid = ens.every(x => x.valid);
+
+    const froms = ens.map(x => x.from);
+    const first = Math.min(...froms);
+    x.from = time(ms(first));
+
+    if (x.valid) {
+      const tos = ens.map(x => x.to);
+      const last = Math.max(...tos);
+      x.to = time(ms(last));
+    } else {
+      x.to = "XX:XX";
+    }
+
+    const valid = ens.filter(x => x.valid);
+    if (valid.length === 0) {
+      x.duration = "Xh XXm";
+    } else {
+      const durations = valid.map(x => ms(x.to) - ms(x.from));
+      const sum = durations.reduce((x, y) => x + y, 0);
+      x.duration = duration(sum);
+    }
+
+    x.entries = [];
+    for (const en of ens) {
+      const y = {};
+      y.from = time(ms(en.from));
+
+      y.valid = en.valid;
+
+      if (y.valid) {
+        y.to = time(ms(en.to));
+        y.duration = duration(ms(en.to) - ms(en.from));
+      } else {
+        y.to = "XX:XX";
+        y.duration = "Xh XXm";
+      }
+
+      x.entries.push(y);
+    }
+
+    ll.unshift(x);
   }
-  return ll.reverse();
+  return ll;
 }
 
-module.exports = { entryList, testables: { diff, date } };
+module.exports = { entryList, testables: { duration, date, time } };
