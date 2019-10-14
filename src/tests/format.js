19d003f4782baa403eaa78e1de17a6cc188109e1
Autoclave <autoclave@firemail.cc>
2019-10-14 12:08:18 +0200

    Update tests

--- src/tests/format.js
+++ src/tests/format.js
@@ -1,32 +1,87 @@
 const tap = require("tap");
 const format = require("../utils/format");
 
-tap.equal(format.testables.date(1561556049000), "26.06.2019 15:34");
-tap.equal(format.testables.date(1560166643000), "10.06.2019 13:37");
-tap.equal(format.testables.date(1546297200000), "1.01.2019 00:00");
+tap.test("date and time", test => {
+  test.equal(format.testables.date(1561556049000), "26.06.2019");
+  test.equal(format.testables.time(1561556049000), "15:34");
+  test.equal(format.testables.date(1560166643000), "10.06.2019");
+  test.equal(format.testables.time(1560166643000), "13:37");
+  test.equal(format.testables.date(1546297200000), "1.01.2019");
+  test.equal(format.testables.time(1546297200000), "00:00");
+  test.end();
+});
 
-tap.equal(format.testables.diff(1560950261138, 1560949971149), "0h 04m 49s");
-tap.equal(format.testables.diff(1 + 5 * 60 * 60 * 1000, 1), "5h 00m 00s");
-tap.equal(format.testables.diff(1 + 500 * 60 * 60 * 1000, 1), "500h 00m");
+tap.test("duration", test => {
+  test.equal(format.duration(5 * 60 * 60 * 1000), "5h 00m");
+  test.equal(format.duration(30 * 1000), "0h 00m");
+  test.equal(format.duration(-30 * 1000), "0h 01m");
+  test.equal(format.duration(90 * 1000), "0h 01m");
+  test.equal(format.duration(-90 * 1000), "0h 02m");
+  test.equal(format.duration(-600 * 1000), "0h 10m");
+  test.end();
+});
 
-const input = [
-  { time: 1560954343558, type: "CLOCK_IN" },
-  { time: 1561552340579, type: "CLOCK_OUT" },
-  { time: 1561553645552, type: "CLOCK_IN" },
-  { time: 1561553646614, type: "CLOCK_OUT" },
-  { time: 1561557144846, type: "CLOCK_IN" }
-];
-
-const currentTime = +new Date();
-
-const output = [
-  {
-    in: "26.06.2019 15:52",
-    out: null,
-    duration: format.testables.diff(currentTime, 1561557144846)
-  },
-  { in: "26.06.2019 14:54", out: "26.06.2019 14:54", duration: "0h 00m 01s" },
-  { in: "19.06.2019 16:25", out: "26.06.2019 14:32", duration: "166h 06m" }
-];
-
-tap.same(format.entryList(input), output);
+tap.test("entryList", test => {
+  test.same(
+    format.entryList({
+      "1571004000": [
+        { id: 105, from: 1571043673, to: 1571043674, valid: true },
+        { id: 106, from: 1571043678, to: 1571044420, valid: true }
+      ]
+    }),
+    [
+      {
+        date: "14.10.2019",
+        valid: true,
+        from: "11:01",
+        to: "11:13",
+        duration: "0h 12m",
+        entries: [
+          { from: "11:01", valid: true, to: "11:01", duration: "0h 00m" },
+          { from: "11:01", valid: true, to: "11:13", duration: "0h 12m" }
+        ]
+      }
+    ]
+  );
+  test.same(
+    format.entryList({
+      "1571004000": [
+        { id: 105, from: 1571043673, to: 1571043674, valid: true },
+        { id: 106, from: 1571043678, to: 1571044420, valid: false }
+      ]
+    }),
+    [
+      {
+        date: "14.10.2019",
+        valid: false,
+        from: "11:01",
+        to: "XX:XX",
+        duration: "0h 00m",
+        entries: [
+          { from: "11:01", valid: true, to: "11:01", duration: "0h 00m" },
+          { from: "11:01", valid: false, to: "XX:XX", duration: "Xh XXm" }
+        ]
+      }
+    ]
+  );
+  test.same(
+    format.entryList({
+      "1571004000": [
+        { id: 105, from: 1571043673, to: 1571043674, valid: false }
+      ]
+    }),
+    [
+      {
+        date: "14.10.2019",
+        valid: false,
+        from: "11:01",
+        to: "XX:XX",
+        duration: "Xh XXm",
+        entries: [
+          { from: "11:01", valid: false, to: "XX:XX", duration: "Xh XXm" }
+        ]
+      }
+    ]
+  );
+  test.end();
+});
